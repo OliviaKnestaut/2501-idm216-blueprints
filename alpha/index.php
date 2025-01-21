16424d83909a88fc47d6d2cd60235e20ef134bc2
Olivia Knestaut <olivia.knestaut@gmail.com>
2025-01-21 10:49:06 -0500

    Fixing stupid mistake

--- alpha/index.php
+++ alpha/index.php
@@ -7,8 +7,10 @@
     <link rel="stylesheet" href="../css/alpha_styles.css">
     <link rel="icon" href="">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+</head>
 
-
+<body>
+    
 <!-- -------------------------- REGISTER TOGGLE -------------------------- --> 
 
     <?php
