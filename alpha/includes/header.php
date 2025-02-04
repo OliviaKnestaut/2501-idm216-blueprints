9f88beec4f53f8202aeb4bcc077b60bdf1b242fc
Olivia Knestaut <olivia.knestaut@gmail.com>
2025-02-04 11:49:32 -0500

    Progress on welcome.php and main.php

---
+++ alpha/includes/header.php
@@ -0,0 +1,4 @@
+<header>
+    <img class="header-icon" src="../images/alpha/assets/tracking-icon.svg" alt="">
+    <img class="header-icon" src="../images/alpha/assets/cart-icon.svg" alt="">
+</header>
